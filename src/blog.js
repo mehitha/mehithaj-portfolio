fb2cfd45d0e06787e17a69c61de4bdb6807e3330
mehitha <angelinmehitha@gmail.com>
2025-02-26 22:54:12 +0800

    initial commit

--- src/blog.js
+++ src/blog.js
@@ -1,28 +1,44 @@
+import { getAllByDisplayValue } from "@testing-library/dom";
 import "./index.css";
+import { useEffect } from "react";
 
 const Blog = () => {
+    useEffect(() => {
+        document.querySelector(".hero").classList.add("animated");
+        document.querySelector(".header").classList.add("nav-animated");
+        document.querySelector(".three").classList.add("animated");
+    }, []);
+
     return (
         <div className="container">
+            {/* Background Animation */}
+            <div className="animated-bg"></div>
+            <div className="floating-circles">
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+            </div>
+
             {/* Header */}
             <nav className="header">
-                <h1 className="logo">J Mehitha</h1>
+                <h1 className="logo" >JMehitha</h1>
                 <ul className="nav-menu">
                     <li><a href="/home">Home</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/contact">Cont</a></li>
                     <li><a href="https://github.com/mehitha" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-                    <li><a href="https://instagram.com/mehitha_jf" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                    <li><a href="https://instagram.com/mehitha_jf" target="_blank" rel="noopener noreferrer">Insta</a></li>
                 </ul>
             </nav>
-            
 
             {/* Hero Section */}
             <div className="hero">
                 <h2>Hey, I'M</h2>
                 <h1 className="two">J MEHITHA</h1> 
-                </div>
-                <div className="three" >
-                
-                <p> A backend-focused Web Developer building the backendend of Websites & Web Applications that leads to the success of the overall project.</p>
+            </div>
+
+            <div className="three">
+                <p>A backend-focused Web Developer building the backend of Websites & Web Applications that leads to the success of the overall project.</p>
             </div>
         </div>
     );
